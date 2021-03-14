929c3428c256e04e9220f0bb4584ae723c8f99ad
Richard Cure <richarcu@uk.ibm.com@MacBook-Pro.local>
2021-03-14 19:02:19 +0000

    adding 1st working version of data ingestor script and 1st raw transactions output csv

--- data-ingestor.py
+++ data-ingestor.py
@@ -3,20 +3,46 @@ import sys
 import pandas as pd
 import numpy as np
 import json
+import glob
 
+
+#setup working directory where data is stored and get JSON input files
 DATA_DIR = os.path.join(".","cs-train")
+json_pattern = os.path.join(DATA_DIR,'*.json')
+file_list = glob.glob(json_pattern)
+print("file_list: " + str(file_list))
 
-#for each json file:
-  #read json into pd
-df = pd.read_json(DATA_DIR+"/invoices-2017-11.json")
 
-print(df.head())
+def read_json_input_data(file_list):
 
- #check for inputs
- #correct them
+    #create an empty list to store the data frames
+    dfs = []
 
- #concatenate into 1 dataframe
+    #for each json file:
+    for file in file_list:
+        #read the json
+        print('reading file')
+        data = pd.read_json(file, orient='columns')
+        print('read file')
+        #append data frame to dfs
+        dfs.append(data)
+        print('appended data to dfs')
 
- #index on day
+    #concatenate all the data frames into one df
+    df = pd.concat(dfs)
 
- #aggregate on price and country?
+    #ouput head, tail and shape of concatenated data
+    print(df.head())
+    print(df.tail())
+    print(df.shape)
+
+    #return as a dataframe
+    return df
+
+#call read function, passing in the list of JSON input files
+df = read_json_input_data(file_list)
+
+#export csv data
+df.to_csv('transactions_raw.csv')
+
+print('csv data exported')

f188ac7c8efc82bf30ecf12cb1284fb5ecdd061e
Richard Cure <richarcu@uk.ibm.com@MacBook-Pro.local>
2021-03-14 19:14:55 +0000

    added try except block

--- data-ingestor.py
+++ data-ingestor.py
@@ -7,10 +7,10 @@ import glob
 
 
 #setup working directory where data is stored and get JSON input files
-DATA_DIR = os.path.join(".","cs-train")
+DATA_DIR = os.path.join('.','cs-train')
 json_pattern = os.path.join(DATA_DIR,'*.json')
 file_list = glob.glob(json_pattern)
-print("file_list: " + str(file_list))
+print('file_list: ' + str(file_list))
 
 
 def read_json_input_data(file_list):
@@ -18,26 +18,29 @@ def read_json_input_data(file_list):
     #create an empty list to store the data frames
     dfs = []
 
-    #for each json file:
-    for file in file_list:
-        #read the json
-        print('reading file')
-        data = pd.read_json(file, orient='columns')
-        print('read file')
-        #append data frame to dfs
-        dfs.append(data)
-        print('appended data to dfs')
+    try:
+        #for each json file:
+        for file in file_list:
+            #read the json
+            print('reading file')
+            data = pd.read_json(file, orient='columns')
+            print('read file')
+            #append data frame to dfs
+            dfs.append(data)
+            print('appended data to dfs')
 
-    #concatenate all the data frames into one df
-    df = pd.concat(dfs)
+        #concatenate all the data frames into one df
+        df = pd.concat(dfs)
 
-    #ouput head, tail and shape of concatenated data
-    print(df.head())
-    print(df.tail())
-    print(df.shape)
+        #ouput head, tail and shape of concatenated data
+        print(df.head())
+        print(df.tail())
+        print(df.shape)
 
-    #return as a dataframe
-    return df
+        #return as a dataframe
+        return df
+    except Error as e:
+        print('error reading json', e)
 
 #call read function, passing in the list of JSON input files
 df = read_json_input_data(file_list)
